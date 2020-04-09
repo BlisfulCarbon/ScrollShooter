92bea8c08fe3d9fe83bb7e74d00ed4e44b0552b5
Aleh <blissful.carbon@gmail.com>
2020-04-09 16:30:30 +0300

    Add weapon and player fire

---
+++ Assets/Scripts/Bulet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulet : MonoBehaviour
+{
+    [Header("Movement Variables")] 
+    public float speed;
+    private Rigidbody2D rigidbody;
+
+    [Header("Lifetime")]
+    public float lifetime;
+    private float lifetimeSeconds;
+
+    void Start()
+    {
+        lifetimeSeconds = lifetime;
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(0f, speed);
+    }
+    
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    [Header("Fire Variables")]
+    public Transform firePoint;
+    public GameObject weapon;
+    public float fireWait = 3f;
+    private float fireWaitSeconds;
+    
+    void Start()
+    {
+        fireWaitSeconds = fireWait;
+    }
+    void Update()
+    {
+        fireWaitSeconds -= Time.deltaTime;
+        if (fireWaitSeconds <= 0)
+        {
+            Fire();
+            fireWaitSeconds = fireWait;
+        }
+    }
+
+    public void Fire()
+    {
+        Instantiate(weapon, firePoint.position, Quaternion.identity);
+    }
+}
