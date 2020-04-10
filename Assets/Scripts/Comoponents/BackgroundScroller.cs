dbf98f643689c62508f2976b65d924f33360e76f
Aleh <blissful.carbon@gmail.com>
2020-04-10 12:13:02 +0300

    Add scroll background

---
+++ Assets/Scripts/Comoponents/BackgroundScroller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float speed = 0.2f;
+    public Renderer bgRend;
+    private void Update()
+    {
+        bgRend.material.mainTextureOffset = new Vector2(0, Time.time * speed);
+    }
+}
