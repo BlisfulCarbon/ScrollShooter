fba46c3ff19b01f26d4425509110d7bfa818e4d3
Aleh <blissful.carbon@gmail.com>
2020-04-13 18:11:07 +0300

    Destroy objects at the bottom of the screen

---
+++ Assets/Scripts/Comoponents/DestroyWhenCollisionBottom.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+ 
+ public class DestroyWhenCollisionBottom : MonoBehaviour
+ {
+     private void OnTriggerEnter2D(Collider2D other)
+     {
+         Debug.Log("Collision triger!");
+         if(other.GetComponent<Bottom>() != null)
+             Destroy(this.gameObject);
+     }
+ }
