23f75c548d793ddc669458fda8b46b64c9ca561a
Aleh <blissful.carbon@gmail.com>
2020-04-13 18:21:26 +0300

    Add simple enemies spawn

---
+++ Assets/Scripts/Comoponents/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnTimer = 2;
+
+    public List<EnemyWave> stage;
+    public float timer;
+
+    public GameObject simpleEnemy;
+    public GameObject hardEnemy;
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnTimer)
+        {
+            float waveNumber = Random.Range(1, 3);
+            switch(waveNumber)
+            {
+                case 1:
+                    SpawnSimpleWave();
+                    break;
+                case 2:
+                    SpawnHardWave();
+                    break;
+            }
+            timer = 0;
+        }
+    }
+
+    private void SpawnSimpleWave()
+    {
+        Instantiate(simpleEnemy, new Vector3(transform.position.x - 1, transform.position.y), Quaternion.identity);
+        Instantiate(simpleEnemy, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+        Instantiate(simpleEnemy, new Vector3(transform.position.x + 0.8f, transform.position.y), Quaternion.identity);
+    }
+
+    private void SpawnHardWave()
+    {
+        Instantiate(hardEnemy, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+    }
+}
