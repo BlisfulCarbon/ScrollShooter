d8a23513d3bc0029493f615596a437eaa7e79c49
Aleh <blissful.carbon@gmail.com>
2020-04-13 08:18:27 +0300

    Update Unity files

---
+++ Assets/Scripts/Model/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
