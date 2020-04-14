f948b0369db37a8fe4ebc302e985d5cc29c92a66
Aleh <blissful.carbon@gmail.com>
2020-04-14 18:56:08 +0300

    Add when enemy smash into player destroy player

---
+++ Assets/Scripts/Comoponents/DestroyWhenCollisionEnemy.cs
@@ -0,0 +1,17 @@
+﻿using ScrollShooter.Events;
+using ScrollShooter.Interfaces;
+using UnityEngine;
+
+public class DestroyWhenCollisionEnemy : Actor
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Plyer trriger");
+        if (other.GetComponent<Enemy>() == null)
+            return;
+
+        Debug.Log("It's enemy");
+        EventAggregator.enemySmashIntoPlayer.Publish(this);
+        Destroy(this.gameObject);
+    }
+}
